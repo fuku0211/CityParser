6cad0dca9da8a99f02831119e02bc9a034e7aed5
fuku0211 <fukuzumi0211@gmail.com>
2019-11-13 07:16:52 +0900

    Fix: fixed route project

---
+++ src/geometry/shapes.py
@@ -0,0 +1,31 @@
+import shapefile
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+
+
+class Shape:
+    def __init__(self, shp_path):
+        self.path = shp_path
+        print("loading shape file")
+        self.bldg = self._load_parts("tatemono.shp")
+        self.road = self._load_parts("road.shp")
+        self.side = self._load_parts("hodou.shp")
+
+    def _load_parts(self, file_name):
+        file_path = self.path / Path(file_name)
+
+        shp_points_all = []
+        with shapefile.Reader(str(file_path)) as src:
+            shps = src.shapes()
+            for shp in tqdm(shps, desc=f"{file_name}"):
+                # 線の内部に線がある場合は外の線と中の線を区別する
+                if len(shp.parts) > 1:
+                    parts_idx = shp.parts.append(len(shp.points) - 1)
+                    for i in range(0, len(shp.points) - 1, 2):
+                        shp_points_all.append(
+                            shp.points[parts_idx[i] : parts_idx[i + 1]]
+                        )
+                else:
+                    shp_points_all.append(np.asarray(shp.points, dtype="float32"))
+        return shp_points_all
--- src/route.py
+++ src/route.py
@@ -7,16 +7,23 @@ from operator import itemgetter
 import numpy as np
 import pyproj
 import random
+from geometry.shapes import Shape
+from matplotlib.collections import PolyCollection
 
 
 def _parse_gps_data(gpsdata):
     # TODO: 標高=楕円体高であってるかわからない
     lat, lon, dire, ht = map(float, itemgetter(3, 5, 8, 33)(gpsdata))
-    lat /= 100
-    lon /= 100
+    # 欠損値に対する処理
     if lat < 0 or lon < 0:
         x, y = None, None
+    # dddmm.mmmm表記になっているのを(度数+分数/60)でddd.dddd表記にする
+    # http://lifelog.main.jp/wordpress/?p=146
     else:
+        dd_lat, mm_lat = divmod(lat / 100, 1)
+        dd_lon, mm_lon = divmod(lon / 100, 1)
+        lat = dd_lat + mm_lat * 100 / 60
+        lon = dd_lon + mm_lon * 100 / 60
         y, x = TRANSFORMER.transform(lat, lon)
     return (x, y, dire, ht)
 
@@ -35,16 +42,30 @@ def generate_random_color(n):
 
 class Mapbbox:
     def __init__(self):
-        self.min_x = 0
-        self.min_y = 0
-        self.max_x = 0
-        self.max_y = 0
+        self.min_x = None
+        self.min_y = None
+        self.max_x = None
+        self.max_y = None
 
     def update(self, coord_x, coord_y):
-        self.min_x = min(coord_x)
-        self.min_y = min(coord_y)
-        self.max_x = max(coord_x)
-        self.max_y = max(coord_y)
+        min_x = min(coord_x)
+        min_y = min(coord_y)
+        max_x = max(coord_x)
+        max_y = max(coord_y)
+        try:
+            if min_x < self.min_x:
+                self.min_x = min_x
+            if min_y < self.min_y:
+                self.min_y = min_y
+            if max_x > self.max_x:
+                self.max_x = max_x
+            if max_y > self.max_y:
+                self.max_y = max_y
+        except TypeError:
+            self.min_x = min_x
+            self.min_y = min_y
+            self.max_x = max_x
+            self.max_y = max_y
 
     def offsetted(self, rate=0.1):
         offset_x = (self.max_x - self.min_x) * rate
@@ -58,19 +79,34 @@ class Mapbbox:
 
 
 def visualize_route(args):
-    site_path = Path("data", "hdf5", args.site)
-    gps_path = site_path / Path("gps.hdf5")
+    date_path = Path("data", "hdf5", args.site)
+    shape_path = Path("data", "shp", args.site)
+    gps_path = date_path / Path("gps.hdf5")
+    site_shps = Shape(shape_path)
 
     fig = plt.figure()
     bbox = Mapbbox()
     ax = fig.add_subplot(1, 1, 1)
+    # 敷地地図を描画
+    print("drawing shapes")
+    # for poly_categ in [site_shps.bldg]:
+    #     coll = PolyCollection(poly_categ)
+    #     ax.add_collection(coll)
+    for line_categ in [site_shps.road]:
+        for points in tqdm(line_categ):
+            points = points.T
+            ax.plot(points[0, :], points[1, :], color="g", lw=1)
+            bbox.update(points[0, :], points[1, :])
+
+    # 移動ルートを描画
+    print("drawing routes")
     with h5py.File(str(gps_path), "r") as fg:
         for date in args.date:
             coord_x = []
             coord_y = []
 
             frame_count = len(fg[date].keys())
-            for f in trange(frame_count):
+            for f in trange(frame_count, desc=f"{date}"):
                 c_x, c_y, dire, ht = _parse_gps_data(fg[date][str(f)])
                 if c_x is None and c_y is None:
                     continue
@@ -112,6 +148,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    TRANSFORMER = pyproj.Transformer.from_proj(6668, 6677)
+    TRANSFORMER = pyproj.Transformer.from_crs("EPSG:6668", "EPSG:30169")
+
     if hasattr(args, "handler"):
         args.handler(args)
