d9eef2fb5205f6b718f7c23930e8c33cba078537
fuku0211 <fukuzumi0211@gmail.com>
2019-11-12 17:57:27 +0900

    Add: add command to visualize walking route

---
+++ src/route.py
@@ -0,0 +1,83 @@
+import argparse
+import matplotlib.pyplot as plt
+import h5py
+from pathlib import Path
+from tqdm import tqdm, trange
+from operator import itemgetter
+import numpy as np
+import pyproj
+
+
+def _parse_gps_data(gpsdata):
+    # TODO: 標高=楕円体高であってるかわからない
+    lat, lon, dire, ht = map(float, itemgetter(3, 5, 8, 33)(gpsdata))
+    lat /= 100
+    lon /= 100
+    if lat < 0 or lon < 0:
+        x, y = None, None
+    else:
+        y, x = TRANSFORMER.transform(lat, lon)
+    return (x, y, dire, ht)
+
+
+def rotation(x, t):
+    t = np.deg2rad(t)
+    a = np.array([[np.cos(t), -np.sin(t)], [np.sin(t), np.cos(t)]])
+    ax = np.dot(a, x)
+    return ax
+
+
+def visualize_route(args):
+    site_path = Path("data", "hdf5", args.site)
+    gps_path = site_path / Path("gps.hdf5")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    with h5py.File(str(gps_path), "r") as fg:
+        for date in args.date:
+            coord_x = []
+            coord_y = []
+            # arrow_x = []
+            # arrow_y = []
+
+            frame_count = len(fg[date].keys())
+            for f in trange(frame_count):
+                c_x, c_y, dire, ht = _parse_gps_data(fg[date][str(f)])
+                if c_x is None and c_y is None:
+                    continue
+                coord_x.append(c_x)
+                coord_y.append(c_y)
+                dire = 90 - dire if dire <= 90 else 360 - (dire - 90)
+                # vec = rotation(np.array([0.2, 0]), dire)
+                # arrow_x.append(vec[0])
+                # arrow_y.append(vec[1])
+                ax.text(c_x, c_y, str(f), fontsize=10)
+            ax.scatter(coord_x, coord_y, s=10)
+            # ax.scatter(arrow_x, arrow_y, s=1.5, c='r')
+            # ax.quiver(coord_x, coord_y, arrow_x, arrow_y, units='xy', width=0.01)
+
+    ax.set_xlim([min(coord_x), max(coord_x)])
+    ax.set_ylim([min(coord_y), max(coord_y)])
+    ax.set_aspect("equal")
+    plt.show()
+
+
+if __name__ == "__main__":
+    # コマンドライン用
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument("-s", "--site", required=True)
+    parent_parser.add_argument("-d", "--date", required=True, nargs="*")
+
+    # 各コマンドの設定
+    vis_parser = subparsers.add_parser("vis", parents=[parent_parser])
+    vis_parser.set_defaults(handler=visualize_route)
+
+    args = parser.parse_args()
+
+    TRANSFORMER = pyproj.Transformer.from_proj(6668, 6677)
+    if hasattr(args, "handler"):
+        args.handler(args)
